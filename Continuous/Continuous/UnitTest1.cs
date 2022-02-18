8549325dc9e51f802f1d02583e10cf968f09b930
slavu <slavutich0801@gmail.com>
2022-02-18 23:25:52 +0200

    commit

--- Continuous/Continuous/UnitTest1.cs
+++ Continuous/Continuous/UnitTest1.cs
@@ -40,5 +40,6 @@ namespace Continuous
             Action action = () => WorkingMethod.CheckCompoundInterest(a, b, c);
             Assert.Throws<ArgumentOutOfRangeException>(action);
         }
+        // Testing again
     }
 }
